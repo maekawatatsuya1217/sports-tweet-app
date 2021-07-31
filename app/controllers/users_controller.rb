3f4f6eb43aa0bb3a9c3d059ee89161c4179be898
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-01 08:41:14 +0900

    ユーザー詳細ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: :show
+
+    def show
+        @tweets = @user.tweet.with_attached_image.order("created_at DESC")
+    end
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
 end
