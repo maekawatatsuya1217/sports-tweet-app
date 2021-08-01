8908c71de9f4af049e24b324b90dd77455c077a9
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-01 19:23:21 +0900

    ツイートテストコードの実装

---
+++ spec/factories/tweets.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :tweet do
+    title       {'スポーツ'}
+    catch_copy  {'キャッチコピー'}
+    article     {'ツイート本文'}
+
+    association :user
+
+    after(:build) do |tweet|
+      tweet.image.attach(io: File.open('app/assets/images/shea.png'), filename: 'shea.png')
+    end
+  end
+end
