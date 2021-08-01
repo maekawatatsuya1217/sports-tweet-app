cedf0d243d8142016787989e301178ab86062c30
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-01 09:44:17 +0900

    バリデーションの記述

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,8 @@
 class Tweet < ApplicationRecord
+    with_options presence: true do
+        validates :title
+        validates :catch_copy
+        validates :article
+        validates :image
+    end
 end
