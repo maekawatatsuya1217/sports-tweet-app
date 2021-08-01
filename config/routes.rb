373642d7553d3672458fd090c6869e437bca9454
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-01 16:57:00 +0900

    詳細表示機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
   resources :users, only: [:show, :edit, :update]
-  resources :tweets, only: [:new, :create]
+  resources :tweets, only: [:new, :create, :show]
 end
