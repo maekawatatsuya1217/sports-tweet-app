b27829bd5843266564664e6266f2ac9149d2ae55
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-03 14:05:39 +0900

    HTMLを挿入

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,3 +1,14 @@
+const buildHTML = (XHR) => {
+    const comment = XHR.response.comment;
+    const user = XHR.response.user;
+    const html = `
+          <p>
+            <strong><a href="/users/${comment.user_id}">${user.name}</a>：</strong>
+            ${comment.text}
+          </p>`;
+    return html;
+};
+
 function post(){
     const submit = document.getElementById("submit");
     submit.addEventListener("click", (e) =>{
@@ -5,11 +16,21 @@ function post(){
         const form = document.getElementById("form");
         const tweet = document.getElementById("tweet");
         const tweetId = tweet.getAttribute("value");
-        const formDate = new formDate(form);
+        const formData = new FormData(form);
         const XHR = new XMLHttpRequest();
         XHR.open("POST", `/tweets/${tweetId}/comments`, true);
         XHR.responseType = "json";
         XHR.send(formData);
+        XHR.onload = () => {
+            if (XHR.status != 200) {
+                alert(`Error ${XHR.status}: ${XHR.statusText}`);
+                return null;
+              };
+              const list = document.getElementById("list");
+              const formText = document.getElementById("content");
+          list.insertAdjacentHTML("afterend", buildHTML(XHR));
+          formText.value = "";
+        };
     });
 };
 
