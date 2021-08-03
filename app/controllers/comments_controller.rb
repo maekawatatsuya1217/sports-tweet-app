0120e5159df335116e3f4da17505a984de71ef08
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-03 11:02:04 +0900

    コントローラーやアソシエーションの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,6 @@ class CommentsController < ApplicationController
     private
 
     def comment_params
-        params.require(:comment).permit(:text).merge(user_id: :current_user.id, tweet_id: params[:tweet_id])
+        params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
 
     validates :text, presence: true
 
-    balongs_to :user
+    belongs_to :user
     belongs_to :tweet
     
 end
