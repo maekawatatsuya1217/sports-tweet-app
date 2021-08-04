1bbf001378b228801dd27d75e3348c150a8a9821
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-04 11:55:40 +0900

    category.rbを編集

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveHash::Base
     self.data = [
-        { id: 1, name: '--'}
-        { id: 2, name: 'サッカー'}
+        { id: 1, name: '--'},
+        { id: 2, name: 'サッカー'},
         { id: 3, name: '野球' },
         { id: 4, name: 'テニス' },
         { id: 5, name: 'バドミントン' },
@@ -9,9 +9,9 @@ class Category < ActiveHash::Base
         { id: 7, name: '陸上競技' },
         { id: 8, name: '水泳' },
         { id: 9, name: '体操' },
-        { id: 10, name: '武道・格闘技' }
-        { id: 11, name: '雪上スポーツ'}
-        { id: 12, name: '自転車競技'}
+        { id: 10, name: '武道・格闘技' },
+        { id: 11, name: '雪上スポーツ'},
+        { id: 12, name: '自転車競技'},
         { id: 13, name: 'その他'}
     ]
 
