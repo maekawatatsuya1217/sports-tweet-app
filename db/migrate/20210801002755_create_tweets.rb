8f3b166dfa486bce0c0f3de15ff819f0e2a5279b
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-04 11:21:31 +0900

    マイグレーションを編集

--- db/migrate/20210801002755_create_tweets.rb
+++ db/migrate/20210801002755_create_tweets.rb
@@ -1,10 +1,11 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string :title,      null: false
-      t.string :catch_copy, null: false
-      t.text :article,      null:false
-      t.references :user,   null: false, foreign_key: true
+      t.string :title,        null: false
+      t.string :catch_copy,   null: false
+      t.text :article,        null:false
+      t.integer :category_id, null: false
+      t.references :user,     null: false, foreign_key: true
       t.timestamps
     end
   end
