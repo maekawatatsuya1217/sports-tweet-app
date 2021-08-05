14480e84abdb7a5ff51b9a2e304d11df2d2de4b7
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-05 10:00:40 +0900

    クリック時に検索機能をバーを表示

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function(){
     const pullDownButton = document.getElementById("search")
+    const pullDownParents = document.getElementById("pull-down")
     pullDownButton.addEventListener('click', function(){
-        
+        pullDownParents.setAttribute("style", "display:flex;")
     })
 })
