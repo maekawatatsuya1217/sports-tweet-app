490590ca8bd8cc97c69e632d25f2855589e506f3
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-10 09:51:38 +0900

    カテゴリーの追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveHash::Base
     self.data = [
-        { id: 1, name: 'カテゴリーを選択'},
+        { id: 1, name: '--'},
         { id: 2, name: 'サッカー'},
         { id: 3, name: '野球' },
         { id: 4, name: 'テニス' },
@@ -9,10 +9,18 @@ class Category < ActiveHash::Base
         { id: 7, name: '陸上競技' },
         { id: 8, name: '水泳' },
         { id: 9, name: '体操' },
-        { id: 10, name: '武道・格闘技' },
+        { id: 10, name: '格闘技' },
         { id: 11, name: '雪上スポーツ'},
         { id: 12, name: '自転車競技'},
-        { id: 13, name: 'その他'}
+        { id: 13, name: '卓球'},
+        { id: 14, name: '弓道'}
+        { id: 15, name: '柔道'}
+        { id: 16, name: 'バレーボール'}
+        { id: 17, name: 'バスケットボール'}
+        { id: 18, name: 'ソフトボール'}
+        { id: 19, name: 'フットボール'}
+        { id: 20, name: 'ラグビー'}
+        { id: 21, name: 'その他'}
     ]
 
     include ActiveHash::Associations
