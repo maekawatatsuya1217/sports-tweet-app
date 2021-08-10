291f1f481f39b3f6b0ae9f065183c71bb94ceec7
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-10 10:54:07 +0900

    アソシエーションの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,8 @@ class User < ApplicationRecord
 
   has_many :tweets
   has_many :comments
-  
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 end
