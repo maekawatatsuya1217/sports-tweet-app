bb577bdb2f94e0308ccf2f158c789f8b12b28912
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-08-11 10:37:56 +0900

    コントローラーの修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,10 +4,8 @@ class RelationshipsController < ApplicationController
     def create
         following = current_user.follow(@user)
         if following.save
-            flash[:success] = '#{current_user.name}さんをフォローしました。'
             redirect_to @user
         else
-            flash.now[:alert] = '#{current_user.name}さんをフォローできませんでした。'
             redirect_to @user
         end
     end
@@ -15,10 +13,8 @@ class RelationshipsController < ApplicationController
     def destroy
         following = current_user.unfollow(@user)
         if following.destroy
-            flash[:alert] = '#{current_user.name}さんのフォローを解除しました。'
             redirect_to @user
         else
-            flash[:alert] = '#{current_user.name}さんをフォロー解除できませんでした。'
             redirect_to @user
         end
     end
@@ -26,6 +22,6 @@ class RelationshipsController < ApplicationController
     private
 
     def set_user
-        @user = User.find(params[:relationships][:follow_id])
+        @user = User.find(params[:follow_id])
     end
 end
